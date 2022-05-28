3583ab391731207dfb54a25e9c757a8cbc406f8c
jkohl15 <87139234+jkohl15@users.noreply.github.com>
2022-05-27 20:20:53 -0400

    Create s3.py

---
+++ gallery/tools/s3.py
@@ -0,0 +1,43 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+def create_bucket(bucket_name, region=None):
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
+    except ClientError as e:
+            logging.error(e)
+            return False
+    return True
+
+def put_object(bucket_name, key, value):
+    try:
+        s3_client = boto3.client('s3')
+        s3_client.put_object(Bucket=bucket_name, Key=key, Body=value)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+def get_object(bucket_name, key):
+    try:
+        s3_client = boto3.client('s3')
+        result = s3_client.get_object(Bucket=bucket_name, Key=key)
+    except ClientError as e:
+        logging.error(e)
+        return None
+    return result
+
+def main():
+    #create_bucket('edu.au.cc.jeff.image-gallery', 'us-east-2')
+    # put_object('edu.au.cc.jeff.image-gallery', 'banana', 'green')
+    print(get_object('edu.au.cc.jeff.image-gallery', 'banana')['Body'].read())
+
+if __name__ == '__main__':
+    main()
